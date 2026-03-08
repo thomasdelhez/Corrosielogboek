4a8c47629fe531f5b4ef235e8e0478e25c00b0ad
Thomas Delhez <thomasdelhez@live.nl>
2026-03-08 10:32:52 +0100

    Protect main menu route behind login guard

--- corosiemanager/frontend/src/app/app.routes.ts
+++ corosiemanager/frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { CORROSION_ROUTES } from './areas/corrosion/corrosion.routes';
 import { HomePage } from './core/pages/home.page';
 import { LoginPage } from './core/pages/login.page';
+import { requireLoginGuard } from './core/security/guards/auth.guards';
 
 export const routes: Routes = [
-  { path: '', component: HomePage },
+  { path: '', component: HomePage, canActivate: [requireLoginGuard] },
   { path: 'login', component: LoginPage },
   ...CORROSION_ROUTES,
 ];
